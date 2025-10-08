92a6d241046f048d97d308a485febd04a0039592
anujYadav <anujvns28@gmail.com>
2025-10-08 17:11:35 +0530

    mail error fiexed

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,32 +1,30 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
-  console.log("mail pass  ", process.env.MAIL_PASS);
-  console.log("mail host  ", process.env.MAIL_HOST);
-  console.log("mail user  ", process.env.MAIL_USER);
-
   try {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port: 587, // Brevo SMTP port
+      secure: false, // false for 587
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      secure: false,
     });
 
     let info = await transporter.sendMail({
-      from: `"Studynotion " <${process.env.MAIL_USER}>`, // sender address
-      to: `${email}`, // list of receivers
-      subject: `${title}`, // Subject line
-      html: `${body}`, // html body
+      from: '"Studynotion" <dummyanuj80@gmail.com>', // verified Brevo email
+      to: email,
+      subject: title,
+      html: body,
     });
 
+    console.log("Mail sent:", info);
     return info;
   } catch (error) {
-    console.log(error.message);
-    return error.message;
+    console.error("Mail send error:", error);
+    return error;
   }
-}
+};
 
-module.exports = mailSender
+module.exports = mailSender;
