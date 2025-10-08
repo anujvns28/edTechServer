cbb39ac8c4ac73a2a8c9dbdd15acdac3a7fea475
anujYadav <anujvns28@gmail.com>
2025-10-08 16:07:30 +0530

    updated

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer")
 
 const mailSender = async (email, title, body) => {
- 
+  console.log("mail pass  ", process.env.MAIL_PASS);
+  console.log("mail host  ", process.env.MAIL_HOST);
+  console.log("mail user  ", process.env.MAIL_USER);
+
   try {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
@@ -10,19 +13,19 @@ const mailSender = async (email, title, body) => {
         pass: process.env.MAIL_PASS,
       },
       secure: false,
-    })
+    });
 
     let info = await transporter.sendMail({
-      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER}>`, // sender address
+      from: `"Studynotion " <${process.env.MAIL_USER}>`, // sender address
       to: `${email}`, // list of receivers
       subject: `${title}`, // Subject line
       html: `${body}`, // html body
-    })
-    
-    return info
+    });
+
+    return info;
   } catch (error) {
-    console.log(error.message)
-    return error.message
+    console.log(error.message);
+    return error.message;
   }
 }
 
